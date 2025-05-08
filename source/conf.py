cbd9fc64c8d2a34dd7120387e7e45c02ea69625a
Giankarlo <giankarlosverko2021@gmail.com>
2025-05-08 13:57:59 -0400

    Update Sphinx config and RTD setup: theme, extensions

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,17 @@ release = '1.0'
 
 extensions = [
      'myst_parser',
+     'sphinx_copybutton',
+     'sphinxcontrib.plantuml',
+     'sphinxcontrib.spelling',
 ]
+
+# Running the Spell Checker:
+# To check spelling in your documentation, use the following command in terminal when in the proper directory for your sphinx project:
+# sphinx-build -b spelling source/ build/spelling
+
+# Language for spell checker extension
+spelling_lang = 'en_US'
 
 # Support both .rst and .md source files
 source_suffix = {
@@ -33,5 +43,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
+html_theme_options = {
+    "navigation_with_keys": True,
+}
+
+
 html_static_path = ['_static']
